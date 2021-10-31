4686d6e721f77ccd89b06c0fee4d7680c15259f7
myazakky <nimuc@outlook.com>
2021-11-01 08:57:45 +0900

    解が定まってる時に他の解が出てくるバグ修正

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -6,6 +6,12 @@ data Term = Variable String | Atom String deriving (Show)
 
 type Assigment = (Term, Term)
 
+canTakeTerm :: [Assigment] -> Term -> Bool
+canTakeTerm as t = any (\a -> fst a == t) as
+
+takeTerm :: [Assigment] -> Term -> Term
+takeTerm as t = snd $ head $ filter (\a -> fst a == t) as
+
 type Condition = (Bool, [Assigment])
 
 data Literal = Literal
@@ -14,10 +20,12 @@ data Literal = Literal
   }
   deriving (Show)
 
+substitute :: Literal -> [Assigment] -> Literal
+substitute l as = Literal (literalName l) (map (\t -> if canTakeTerm as t then takeTerm as t else t) (literalArgs l))
+
 instance Eq Term where
-  (==) (Variable _) _ = True
   (==) (Atom a1) (Atom a2) = a1 == a2
-  (==) _ _ = False
+  (==) _ _ = True
 
 (==^) :: Literal -> Literal -> Bool
 (==^) l1 l2 =
@@ -35,9 +43,19 @@ goal (Sentence l _) = l
 findReasons :: [Sentence] -> Literal -> [Sentence]
 findReasons s l = filter ((l ==^) . goal) s
 
+transferVariableName :: Condition -> [Assigment] -> Condition
+transferVariableName c a =
+  (fst c, map (\a' -> if canTakeTerm a (fst a') then (takeTerm a (fst a'), snd a') else a') (snd c))
+
 searchCondition' :: [Sentence] -> Literal -> Sentence -> [Condition]
 searchCondition' premise l1 (SLiteral l2) = [(True, zip (literalArgs l1) (literalArgs l2))]
-searchCondition' _ _ _ = []
+searchCondition' premise l1 (Sentence l2 (CLiteral l3)) =
+  let assigments = zip (literalArgs l2) (literalArgs l1)
+      conditions = searchCondition premise (substitute l3 assigments)
+   in if null conditions
+        then []
+        else map (`transferVariableName` assigments) conditions
+searchCondition' premise l1 _ = []
 
 searchCondition :: [Sentence] -> Literal -> [Condition]
-searchCondition premise proposition = concatMap (searchCondition' premise proposition) (findReasons premise proposition)
+searchCondition premise proposition = filter fst $ concatMap (searchCondition' premise proposition) (findReasons premise proposition)
--- test/TestInterpreter.hs
+++ test/TestInterpreter.hs
@@ -5,7 +5,9 @@ import Test.HUnit
 
 premise =
   [ SLiteral $ Literal "human" [Atom "socrates"], -- human(socrates).
-    SLiteral $ Literal "human" [Atom "iwakura"] -- human(iwakura).
+    SLiteral $ Literal "human" [Atom "iwakura"], -- human(iwakura).
+    SLiteral $ Literal "name" [Atom "iwakura", Atom "lain"], -- human(iwakura).
+    Sentence (Literal "die" [Variable "X"]) (CLiteral $ Literal "human" [Variable "X"]) -- die(X) :- human(X).
   ]
 
 test1 =
@@ -18,6 +20,24 @@ test1 =
         (searchCondition premise (Literal "human" [Variable "Y"]))
     )
 
-tests = TestList [TestLabel "1" test1]
+test2 =
+  TestCase
+    ( assertEqual
+        ""
+        [ (True, [(Variable "Y", Atom "socrates")]),
+          (True, [(Variable "Y", Atom "iwakura")])
+        ]
+        (searchCondition premise (Literal "die" [Variable "Y"]))
+    )
+
+test3 =
+  TestCase
+    ( assertEqual
+        ""
+        [(True, [(Atom "socrates", Atom "socrates")])]
+        (searchCondition premise (Literal "die" [Atom "socrates"]))
+    )
+
+tests = TestList [TestLabel "1" test1, TestLabel "2" test2, TestLabel "3" test3]
 
 main = runTestTT tests
